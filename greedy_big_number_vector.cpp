a708ba7ac8b3a08f9ab859fc1209d17c4a679f12
lush-toner <choijh1027@gmail.com>
2021-01-21 13:38:30 +0900

    2021-01-21-algorithm-practice

--- greedy_big_number_vector.cpp
+++ greedy_big_number_vector.cpp
@@ -1,5 +1,5 @@
 /*
-2021-01-20 예제 3-1 거스름돈 문제 cpp 계산 
+2021-01-20 예제 3-2 큰 수의 법칙 문제 cpp 계산 
 이것이 취업을 위한 코딩 테스트다 with 파이썬 참고
 */
 
@@ -12,12 +12,14 @@ Vector를 이용한 풀이
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 int main(void)
 {
     vector <int> v;
-    int n, m, k, d;
+    int n, m, k, d; // n : 총 개수, m : 총 연산, k : 최대 연속 사용, d : 받을 숫자자
     int arr[5];
     int max, smax;
+    int result = 0;
     
 
     scanf("%d%d%d", &n, &m, &k);
@@ -33,10 +35,7 @@ int main(void)
     max = v[n-1];
     smax = v[n-2];
 
-    for(int i = 0; i < n; i++)
-    {
-        cout << v[i] << " ";
-    }
+    result = max * k * (m / k) + smax * ( m % k );
 
-
+    cout << result << endl;
 }
