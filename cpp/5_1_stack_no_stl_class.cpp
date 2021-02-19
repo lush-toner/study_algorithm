50bc2da9e468508c1862b2f2e95292709b5f9fdb
lush-toner <choijh1027@gmail.com>
2021-02-19 22:37:30 +0900

    2021-02-19 updates

--- cpp/5_1_stack_no_stl_class.cpp
+++ cpp/5_1_stack_no_stl_class.cpp
@@ -4,7 +4,7 @@ Stack은 기본적으로 Last in First out의 구조를 띄고 있음
 가장 마지막에 들어온 자료가 먼저 빠져야 다음 자료가 빠질 수 있다.
 DFS 깊이 우선 탐색에 쓰인다.
 스택은 Top이라는 포인터로 가장 상단의 데이터를 알 수 있다.
-참고 사이트 : https://popcorn16.tistory.com/52
+참고 사이트 : http://blog.naver.com/PostView.nhn?blogId=tipsware&logNo=221212462913&categoryNo=88&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=search
 */
 
 #include <iostream>
@@ -13,82 +13,89 @@ using namespace std;
 class Stack
 {
 private:
-    int top, Maxsize;
-    char *stack;
+    unsigned short m_count; // 스택에 저장된 데이터의 개수
+    unsigned short m_size;  // 스택의 크기
+    short *mp_data;         // 스택으로 사용할 메모리 주소를 가리킬 포인터
 
 public:
-    Stack(int size);
-    bool isFull(), isEmpty();
-    char pop();
-    void push(char element);
-    void print();
-};
-
-// 생성자
-Stack::Stack(int size)
-{
-    Maxsize = size; // Maxsize 및 size는 stack 사이즈
-    stack = new char[Maxsize]; // char를 담고있는 배열 생성
-    top = -1; // index 역할
-}
-
-// return bool
-bool Stack::isFull()
-{
-    if (top == Maxsize - 1) // Maxsize가 5일 때 top이 4가 되면 full
-        return 1;
-    else
-        return 0;
-}
-
-// return bool
-bool Stack::isEmpty()
-{
-    if (top == -1) // top이 -1이면 아무것도 없는 것임
-        return 1;
-    else
-        return 0;
-}
-
-char Stack::pop()
-{
-    if (isEmpty() == 1)
-        cout << "Empty!\n";
-    else
+    Stack() // 생성자 및 멤버 변수 초기화
     {
-        return stack[top--];
+        m_size = 0;
+        m_count = 0;
+        mp_data = NULL;
     }
-        
-}
+    ~Stack() // 객체 소멸자 - 사용하던 스택 메모리를 제거
+    {
+        if (mp_data != NULL) delete[] mp_data;
+    }
 
-void Stack::push(char element)
-{
-    if (isFull() == 1)
-        cout << "Full!\n";
-    else
-        stack[++top] = element;
-}
+    // 스택으로 사용할 메모리를 a_size 크기로 생성
 
-void Stack::print()
-{
-    for (int i = 0; i < top + 1; ++i)
-        cout << stack[i] << endl;
-}
+    void create(unsigned short a_size)
+    {
+        // 크기의 유효성 체크
+        if (a_size > 0 && a_size != m_size)
+        {
+            // 이전에 사용하던 메모리가 있으면 해당 메모리 제거
+            if (mp_data != NULL) delete[] mp_data;
+            // 새로운 크기를 저장하고 메모리를 할당
+            m_size = a_size;
+            mp_data = new short[m_size];
+        }
+    }
+
+    // 스택에 정수 value를 하나 추가할 때 사용하는 함수
+    void push(short a_num)
+    {
+        // 스택에 빈 공간이 있는 경우에만 입력을 받는다.
+        if (m_count < m_size)
+        {
+            // 스택에 새로운 value를 저장한다.
+            *(mp_data + m_count) = a_num;
+            m_count++; // 저장된 갯수를 증가시킨다.
+        }
+        else cout << "Stack이 가득차서 value를 더 이상 넣을 수 없다." << endl;
+    }
+
+    // 스택에서 마지막에 입력된 정수 값을 하나 가져오는 함수
+    // 스택에서 정수 value를 성공적으로 가져오면 1을 반환 실패하면 0을 반환
+    short pop(short *ap_num)
+    {
+        if (m_count == 0) // 스택에 저장된 value가 없는 경우
+        {
+            cout << "Stack에 저장된 value가 없다." << endl;
+            return 0;
+        }
+
+        m_count--; // 스택에 저장된 정수의 갯수를 줄임
+        *ap_num = *(mp_data + m_count); // 마지막에 저장된 정수 값을 복사 (포인터 주소에 m_count만큼 더해서 value를 가져온다음 이를 ap_num에 대입)
+        return 1;
+    }
+
+    // 스택에 있는 값들을 출력하는 함수
+    void showstack()
+    {
+        if (m_count == 0) cout << "Stack에 저장된 value가 없습니다." << endl;
+        else{
+            cout << "Stack에 저장된 value 목록" << endl;
+            for (int i = 0; i < m_count; i++)
+            {
+                cout << i +1 << " : " << *(mp_data + 1) << endl;
+            }
+            cout << "총 " << m_count << "개의 value가 저장되어 있습니다." << endl;
+        }
+    }
+};
 
 int main()
 {
-    Stack stack(5); // 생성자
-    stack.push('a');
-    stack.push('b');
-    stack.push('c');
-    stack.push('d');
-    stack.push('e');
-    stack.push('f'); // Full 
+    Stack stack;  // 스택으로 사용할 변수
+    // 메뉴 항목을 선택할 때 사용할 변수와 value를 입력받을 때 사용할 변수
+    short select_index = 0, temp  = 0;
 
-    stack.pop();
-    // stack.push('f');
-    // stack.print();
+    cout << "Stack의 크기를 입력하세요 : " ;
+    cin >> temp;
+    stack.create(temp);
 
-
-    return 0;
+    
 }
